362a2fb97bb65f714542bf40b667cdd970e2327b
Edward Dinki <ejdinki@gmail.com>
2015-02-11 13:54:38 -0500

    :smiley:

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -2,3 +2,14 @@
 $(function(){
   // Your code goes here
 });
+
+$("#new_comment_button").click(function() {
+    console.log( "You clicked the button!" );
+    $("#new_comment_button").hide();
+
+    $("<form id='new_comment'>"
+    + "<textarea></textarea>"
+    + "<input id='authorName'></input>"
+    + "<input type='submit' value='Create Comment'></input>"
+    + "</form>").appendTo( document.body );
+});

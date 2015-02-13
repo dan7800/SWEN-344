20acb9be2c36ead4bdbf0b586c2d68acd7375982
corbanmailloux <cdm3806@rit.edu>
2015-02-13 12:47:50 -0500

    Completed the OOJS assignment.

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,44 @@
+var dice = [];
+var dieNumber = 0;
+
 $(document).ready(function() {
+  var diceSet = new Dice();
+
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    diceSet.add();
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    diceSet.rollAll();
   });
 });
+
+
+// Dice group object
+var Dice = function() {
+  this.dice = []
+};
+
+Dice.prototype.add = function() {
+  this.dice.push(new Die());
+};
+
+Dice.prototype.rollAll = function() {
+  var index;
+  for (index = 0; index < this.dice.length; ++index) {
+    this.dice[index].roll();
+  }
+};
+
+// Die object
+var Die = function() {
+  this.value = 0;
+  this.id = "die_" + dieNumber.toString();
+  dieNumber++;
+  $('.dice').append('<div class="die" id="' + this.id + '">' + this.value + '</div>');
+};
+
+Die.prototype.roll = function() {
+  this.value = Math.floor((Math.random()*6)+1);
+  document.getElementById(this.id).innerText = this.value.toString();
+};

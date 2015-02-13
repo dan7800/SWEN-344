1b00dabc6efaf8b7cb479378e387d251f7227125
jtf8647 <jtf8647@rit.edu>
2015-02-13 12:18:11 -0500

    Adds comment author, needs comment body

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,11 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  $("#new_comment_button").click(function(){
+	  $("#new_comment").toggle();
+  });
+  $("#new_comment").submit(function(event) {
+	 event.preventDefault();
+	var new_task = $( "input[name='authorName']" ).eq(0).val();
+	$("#comment_list").append('<li>'+new_task+'</li>');
+  });
 });

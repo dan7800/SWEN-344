ba8d3ceb18d375c533ac88d8676d56fecaa26c2b
Locke64 <dmw9070@rit.edu>
2015-02-13 12:05:42 -0500

    completed comment activity

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,24 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  $('#new_comment_button').click( function() {
+	$('#new_comment_button').hide();
+	$('#new_comment').show();
+  } );
+  
+  $('#new_comment').submit( function( event ) {
+	event.preventDefault();
+	$('#new_comment').hide();
+	$('#new_comment_button').show();
+	var comment = $('#new_comment textarea');
+	var author = $('#new_comment #authorName');
+	add_comment( comment.val(), author.val() );
+	comment.val('');
+	author.val('');
+  } );
+  
+  function add_comment( body, author ) {
+	$('#comment_list').append(
+      $('<li/>').html( body ).append(
+        $('<span/>', {class:'author'}).html( author ) ) );
+  };
 });

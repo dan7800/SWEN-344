22dbb9098063248e0118694185170e7d5e5f1101
cpn9025 <cpn9025@rit.edu>
2015-02-13 12:50:07 -0500

    Adding refactor

--- oojs/application.js
+++ oojs/application.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
 
-  var add_dice = function(){
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+  var dice = function(){
+
+    var self = this;
+
+    self.add_dice = function(){
+      console.log("WAT")
+      $('.dice').append('<div class="die">0</div>');
+    }
+
+
+    self.roll_dice = function(){
+      $('.die').each(self.die)
+    }
+
+    self.die = function(k, die){
+      var value = Math.floor((Math.random()*6)+1);
+      $(die).text(value);
+    }
+
   }
 
-  var roll_dice = function(){
-    $('.die').each(die)
-  }
+  var die = new dice()
 
-  var die = function(k, die){
-    var value = Math.floor((Math.random()*6)+1);
-    $(die).text(value);
-  }
+  $('#roller button.add').on('click', die.add_dice);
 
+  $('#roller button.roll').on('click', die.roll_dice);
 
-  $('#roller button.add').on('click', add_dice);
-
-  $('#roller button.roll').on('click', roll_dice);
-  
 });

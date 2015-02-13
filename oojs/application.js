81162d79b9c414d7b735206975746f53ddc164b9
corbanmailloux <cdm3806@rit.edu>
2015-02-13 14:23:31 -0500

    Fixed the objects.

--- oojs/application.js
+++ oojs/application.js
@@ -15,30 +15,30 @@ $(document).ready(function() {
 
 
 // Dice group object
-var Dice = function() {
+function Dice() {
   this.dice = []
-};
 
-Dice.prototype.add = function() {
-  this.dice.push(new Die());
-};
+  this.add = function() {
+    this.dice.push(new Die());
+  };
 
-Dice.prototype.rollAll = function() {
-  var index;
-  for (index = 0; index < this.dice.length; ++index) {
-    this.dice[index].roll();
-  }
+  this.rollAll = function() {
+    var index;
+    for (index = 0; index < this.dice.length; ++index) {
+      this.dice[index].roll();
+    }
+  };
 };
 
 // Die object
-var Die = function() {
+function Die() {
   this.value = 0;
   this.id = "die_" + dieNumber.toString();
   dieNumber++;
   $('.dice').append('<div class="die" id="' + this.id + '">' + this.value + '</div>');
-};
 
-Die.prototype.roll = function() {
-  this.value = Math.floor((Math.random()*6)+1);
-  document.getElementById(this.id).innerText = this.value.toString();
+  this.roll = function() {
+    this.value = Math.floor((Math.random()*6)+1);
+    document.getElementById(this.id).innerText = this.value.toString();
+  };
 };

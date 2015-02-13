d25f7c66bd5029a2e1f2e5e7f22d8dde187d7470
Christopher <cpn9025@rit.edu>
2015-02-13 12:02:40 -0500

    Finished add_comments

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,26 @@
 // Shorthand for document ready
 $(function(){
   // Your code goes here
+
+  $('#new_comment_button').click(function(){
+  	$('#new_comment').css('display' , 'block')
+  })
+
+  $('#new_comment').submit(function(e){
+    e.preventDefault();
+    
+    var author = $('#new_comment :input[name=author]')
+  	var comment = $('#new_comment :input[name=comment]')
+
+  	console.log(author)
+  	console.log(comment)
+
+    var new_comment = '<li>' + comment.val() + '<span class="author">' + author.val() +'</span></li>'
+    $('#comment_list').append(new_comment)
+
+    comment.val(' ')
+    author.val(' ')
+  })
+
+
 });

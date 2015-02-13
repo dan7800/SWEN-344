a6fd9bfe74842f47ec87d4ab8b9a4ecf1bae9342
dhp7825 <dhp7825@rit.edu>
2015-02-13 12:51:49 -0500

    add comments

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,23 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+	$('#new_comment_button').click(function() {
+		$("#new_comment").show();
+		$('#new_comment_button').hide();
+	});
+	$('#new_comment').submit(function() {
+		event.preventDefault();
+		$("#new_comment").hide();
+		$('#new_comment_button').show();
+		
+		var text = $('textarea').val();
+		var author = '<span class="author">' + $("#authorName").val() + '</span>';
+		
+		$("#comment_list").append(
+			$('<li>').append(
+				text +  author
+			 ) );
+			 
+		$('textarea').val('');
+		$('#authorName').val('');
+	});
 });

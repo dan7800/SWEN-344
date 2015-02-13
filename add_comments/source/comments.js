ef94f7dbe4b5d0ff77ac5f3e5b56f198bcf8af4d
jtf8647 <jtf8647@rit.edu>
2015-02-13 12:32:44 -0500

    Adds full comment correctly

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -5,7 +5,8 @@ $(function(){
   });
   $("#new_comment").submit(function(event) {
 	 event.preventDefault();
-	var new_task = $( "input[name='authorName']" ).eq(0).val();
-	$("#comment_list").append('<li>'+new_task+'</li>');
+	var author_task = $( "input[name='authorName']" ).eq(0).val();
+	var comment_task = $( "textarea[name='comment']" ).eq(0).val();
+	$("#comment_list").append('<li>'+comment_task+'<span class="author">'+author_task+'</span></li>');
   });
 });

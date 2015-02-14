c4fd36fadd5e2ef1f69d8b245fa1acea78b1af92
Michael Yeaple <michael@michaelyeaple.com>
2015-02-14 11:14:59 -0500

    Finished OOJS!

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,38 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+/********* OO Code *********/
+var Die = function() {
+  // Create a new die on the page upon construction.
+  this.$el = $('<div class="die">0</div>');
+};
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+Die.prototype.roll = function() {
+  var value = Math.floor((Math.random()*6)+1);
+
+  this.$el.text(value);
+};
+
+function Dice() {};
+
+Dice.dice = [];
+
+Dice.prototype.add = function() {
+  var die = new Die();
+  Dice.dice.push(die);
+
+  $('.dice').append(die.$el);
+};
+
+Dice.prototype.rollAll = function() {
+  $(Dice.dice).each(function(k, die) {
+    die.roll();
   });
+};
+
+/********* Attach functionality to page *********/
+
+$(document).ready(function() {
+  var dice = new Dice();
+  
+  $('#roller button.add').on('click', dice.add);
+
+  $('#roller button.roll').on('click', dice.rollAll);
 });

93a200288a06346978b76bf8fffe59b56b663494
Nsama Chipalo <nsamachipalo@gmail.com>
2015-02-15 15:28:54 -0500

    finished first activity, working on second

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,41 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+var dice_array =[];
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+function die()
+{
+  this.value = 0;
+}
+
+die.roll = function()
+{
+  for(var d in dice_array)
+  {
+    var newValue = Math.floor((Math.random()*6)+1);
+    d.value = newValue;
+  }
+}
+
+die.add = function()
+{
+  console.log("WAT");
+  var aDice = new die();
+  dice_array.push(aDice);
+  $('.dice').append('<div class="die">' + aDice.value + '</div>');
+}
+
+
+$(document).ready(function() {
+
+var addRollerButton = function (){
+  console.log("WAT");
+}
+
+  $('#roller button.add').click(die.add);
+  $('#roller button.roll').click(die.roll);
+
+  //$('#roller button.roll').on('click', function() {
+  //  $('.die').each(function(k, die) {
+  //    var value = Math.floor((Math.random()*6)+1);
+  //    $(die).text(value);
+  //  });
+  //});
 });

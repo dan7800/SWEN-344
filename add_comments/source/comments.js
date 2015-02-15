b7834ecf3cf7825195056afee6301173c73b362e
Chris <celdridge91190@gmail.com>
2015-02-15 18:51:52 -0500

    Finished add_comments activity

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,25 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+	$("#new_comment_button").on("click", function(event){
+	  // Your code goes here
+		  $("#enter_comment").show();
+		  $("#new_comment_button").hide();
+	}); 
+
+	$("#comment_submit").on("click", function(event){
+		console.log("submit button clicked");
+		event.preventDefault();
+		var commentText = $("#comment_text").val();
+		console.log("text: " + commentText);
+		var commentAuthor = $("#comment_submitter").val();
+		console.log("author: " + commentAuthor);
+		$( "<li> " + commentText + 
+			". <span class=\"author\"> " + commentAuthor + " </span>" + 
+		  "</li>" ).appendTo( "#comment_list" );
+		  $("#comment_text").val("");
+		  $("#comment_author").val("");
+		  $("#enter_comment").hide();
+		  $("#new_comment_button").show();
+		  
+	});
 });

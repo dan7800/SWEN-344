03173eb54c266983ae8e393e276a55ce0155e7ed
dhp7825 <dhp7825@rit.edu>
2015-02-16 11:11:49 -0500

    oojs complete

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,22 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+	var dice = [];
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
+  dice.add = function() {
+	$('.dice').append('<div class="die">0</div>');
+  }
+  
+  dice.roll = function() {
+	  $('.die').each(function(k, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
     });
+  }
+
+$(document).ready(function() {  
+  $('#roller button.add').on('click', function() {
+    dice.add();
+  });
+
+  $('#roller button.roll').on('click', function() {
+    dice.roll();
   });
 });

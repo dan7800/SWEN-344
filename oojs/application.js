a7d455a0dc6c39218f0a521479051623fcbf810b
Maddison Hickson <mjhickson@gmai.com>
2015-02-16 02:27:06 -0500

    Both JQuery Activities Completed

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
+  var diceGroup = new DiceGroup();
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    diceGroup.add();
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    diceGroup.rollAll();
   });
 });
+
+function DiceGroup(){
+    this.diceGroup = [];
+    
+    this.add = function(){
+        var dice = new Dice(this.diceGroup.length);
+        this.diceGroup.push(dice);
+        dice.addToHtml();
+    };
+    
+    this.rollAll = function(){
+        for(var i=0; i<this.diceGroup.length; i++){
+            this.diceGroup[i].roll();
+        }
+    };
+};
+
+function Dice(id){
+    this.value = 0
+    this.id = id;
+    this.addToHtml = function(){
+        $('.dice').append('<div class="die" id="dice_'+ this.id +'">' + this.value +'</div>');
+    };
+    
+    this.roll = function() {
+        this.value = Math.floor((Math.random()*6)+1);
+        $("#dice_"+this.id).text(this.value);
+    };
+};

f77f2e3fa4eb82fe8336fb5d9d532abb6d31b615
towhidabsar <mac9908@rit.edu>
2015-02-16 05:29:30 -0500

    Completed the oojs activity

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,14 @@
 // Shorthand for document ready
 $(function(){
   // Your code goes here
+  
+  
+  // The "Roll dice!" button
+  
+  // "Add dice!" button
+  var addComment = function() {
+    var newCommentBox = new OneDice();
+    hand.add(newDice);
+  };
+  $('#roller button.add').on('click', addComment);
 });

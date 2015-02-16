e0ae0ac2a8e5c920aa9d0df181e0d2edbe264a31
deerhit45 <deerhit45@comcast.net>
2015-02-15 20:59:58 -0500

    part 1 complete
    This will let you add comments. submit no author and handle the case
    where the input is incorrect

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,40 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+// Shorthand for document ready
+$(function(){
+
+$("body").append("<form id='new_comment'>"
++ "<textarea id='commentText'></textarea>"
++ "<input id='authorName'></input>"
++ "<input id='submitComment' type='submit' value='Create Comment'></input>"
++ "</form>");
+var form = $("#new_comment");
+form.hide();
+
+
+$( "#new_comment_button" ).click(function() {
+//show the form and hide the button
+var button = $( "#new_comment_button");
+form.show();
+button.hide();
+});
+
+$("#submitComment").click(function() {
+//now check user input if none do nothing
+event.preventDefault();
+var author = $("#authorName").val();
+if (!author){author = "";}
+var inputComment = $("#commentText").val();
+if (inputComment || inputComment.length > 0) {
+$("#comment_list").append("<li>" + inputComment + "<span class='author'>" + author + "</span>"
++ "</li>");
+$("#new_comment").hide();
+$("#new_comment_button").show();
+//empty used values
+$("#authorName").val("");
+$("#commentText").val("");
+}//if
+});
+
+});
 });

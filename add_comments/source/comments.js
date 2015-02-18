6c4e42b91ca900e996d63ef943e133ff6225c76e
jacqueline-faith <jxc4337@rit.edu>
2015-02-18 12:39:46 -0500

    Complete Add_Comments Activity

--- add_comments/source/comments.js
+++ add_comments/source/comments.js
@@ -1,4 +1,20 @@
-// Shorthand for document ready
-$(function(){
-  // Your code goes here
+$(document).ready(function(){
+
+	$("#new_comment").hide();
+
+	$("#new_comment_button").click(function(){
+		$("#new_comment").show();
+	});
+
+	$("#new_comment").submit(function(event){
+		var comment = $('#commentArea').val();
+		var author = $('#authorName').val();
+		if (author && comment) {
+			$('#comment_list').append('<li>' + comment +
+				'<span class="author">' + author + '</span>' +
+				'</li>');
+			event.preventDefault();
+		}; 
+	});
+		
 });

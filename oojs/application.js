e1b2d915d0b7f014eca56af867e04c625a62536b
Chris Farrell <cxf1897@rit.edu>
2015-02-18 12:39:28 -0500

    Didn't push correctly last time.

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,20 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+function die(){}
 
-  $('#roller button.roll').on('click', function() {
+die.roll = function(){
     $('.die').each(function(k, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
-    });
   });
+}
+
+die.add = function(){
+  var new_die = new die();
+  $('.dice').append('<div class="die">0</div>');
+}
+
+$(document).ready(function() {
+
+  $('#roller button.add').on('click', die.add);
+
+  $('#roller button.roll').on('click', die.roll);
 });

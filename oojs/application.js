1b59a59ea351d23a5dbeca4da531e08feb236ad6
jacqueline-faith <jxc4337@rit.edu>
2015-02-19 01:14:38 -0500

    Complete OOJS Activity

--- oojs/application.js
+++ oojs/application.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+  $('#roller button.add').on('click', addDice);
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  $('#roller button.roll').on('click', rollDice);
 });
+
+
+var applicationStart = function() {
+  $('#roller button.add').on('click', addDice);
+  $('#roller button.roll').on('click', rollDice);
+};
+
+var addDice = function() {
+  console.log("WAT")
+  $('.dice').append('<div class="die">0</div>');
+};
+
+var randomPick = function(k, die) {
+    var value = Math.floor((Math.random()*6)+1);
+    $(die).text(value);
+};
+
+var rollDice = function(k, die) {
+    $('.die').each(randomPick);
+};

6eee2a9380562888e1fc52ce801f78a4ba333c36
jjj8097 <jjj8097>
2015-02-20 12:32:11 -0500

    Added WFC Initial Activity

---
+++ wcf_service/SyndicationServiceLibrary1/SyndicationServiceLibrary1/Feed1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SyndicationServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Feed1" in both code and config file together.
+    public class Feed1 : IFeed1
+    {
+        public SyndicationFeedFormatter CreateFeed()
+        {
+            // Create a new Syndication Feed.
+            SyndicationFeed feed = new SyndicationFeed("Feed Title", "A WCF Syndication Feed", null);
+            List<SyndicationItem> items = new List<SyndicationItem>();
+
+            // Create a new Syndication Item.
+            SyndicationItem item = new SyndicationItem("An item", "Item content", null);
+            items.Add(item);
+            feed.Items = items;
+
+            // Return ATOM or RSS based on query string
+            // rss -> http://localhost:8732/Design_Time_Addresses/SyndicationServiceLibrary1/Feed1/
+            // atom -> http://localhost:8732/Design_Time_Addresses/SyndicationServiceLibrary1/Feed1/?format=atom
+            string query = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["format"];
+            SyndicationFeedFormatter formatter = null;
+            if (query == "atom")
+            {
+                formatter = new Atom10FeedFormatter(feed);
+            }
+            else
+            {
+                formatter = new Rss20FeedFormatter(feed);
+            }
+
+            return formatter;
+        }
+    }
+}
---
+++ wcf_service/SyndicationServiceLibrary1/SyndicationServiceLibrary1/IFeed1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SyndicationServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFeed1" in both code and config file together.
+    [ServiceContract]
+    [ServiceKnownType(typeof(Atom10FeedFormatter))]
+    [ServiceKnownType(typeof(Rss20FeedFormatter))]
+    public interface IFeed1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "*", BodyStyle = WebMessageBodyStyle.Bare)]
+        SyndicationFeedFormatter CreateFeed();
+
+        // TODO: Add your service operations here
+    }
+}

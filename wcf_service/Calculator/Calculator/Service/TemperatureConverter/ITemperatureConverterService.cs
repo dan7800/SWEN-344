845928d09528243992eab034daa16ac3d25cd5fb
Morgan Cabral <morgan@ziprecruiter.com>
2015-02-23 01:45:51 -0500

    Added the temperature converter portion of the assignment.

---
+++ wcf_service/Calculator/Calculator/Service/TemperatureConverter/ITemperatureConverterService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Service for converting temperatures to and from celsius and Fahrenheit.
+    /// </summary>
+    [ServiceContract]
+    public interface ITemperatureConverterService
+    {
+        /// <summary>
+        /// Given a temperature in fahrenheit, return the result of converting
+        /// it to celsius.
+        /// </summary>
+        /// <param name="fahrenheit">A temperture in fahrenheit.</param>
+        /// <returns>The result of converting the temperature to celsius.</returns>
+        [OperationContract]
+        double celsius(double fahrenheit);
+
+        /// <summary>
+        /// Given a temperature in celsius, return the result of converting
+        /// it to fahrenheit.
+        /// </summary>
+        /// <param name="celsius">A temperature in celsius.</param>
+        /// <returns>The result of converting the temperature to fahrenheit.</returns>
+        [OperationContract]
+        double fahrenheit(double celsius);
+    }
+}

0b9bf27fc7518cb587ed835c719dd58808bddf46
Dan Krutz <dxkvse@rit.edu>
2015-09-11 11:30:23 -0400

    Added Weather Service API

---
+++ Examples/API/Weather/WebApplication3/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SWEN-344 Example Weather API Page</title>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+    <div>
+    <asp:TextBox ID="txtCity" runat="server" Text="" />
+<asp:Button ID="Button1" Text="Get Weather Information" runat="server" OnClick="GetWeatherInfo" />
+<hr />
+<table id="tblWeather" runat="server" border="0" cellpadding="0" cellspacing="0"
+    visible="false">
+    <tr>
+        <th colspan="2">
+            Weather Information
+        </th>
+    </tr>
+    <tr>
+        <td rowspan="3">
+            <asp:Image ID="imgWeatherIcon" runat="server" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Label ID="lblCity_Country" runat="server" />
+            <asp:Image ID="imgCountryFlag" runat="server" />
+            <asp:Label ID="lblDescription" runat="server" />
+            Humidity:
+            <asp:Label ID="lblHumidity" runat="server" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Temperature:
+            (Min:
+            <asp:Label ID="lblTempMin" runat="server" />
+            Max:
+            <asp:Label ID="lblTempMax" runat="server" />
+                Day:
+            <asp:Label ID="lblTempDay" runat="server" />
+                Night:
+            <asp:Label ID="lblTempNight" runat="server" />)
+        </td>
+    </tr>
+</table>
+    </div>
+    </form>
+
+    <br />
+    <img src="https://c1.staticflickr.com/1/33/61081517_dc34c23a90.jpg" alt="Will is snow?">
+</body>
+</html>
---
+++ Examples/API/Weather/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Web.Script.Serialization;
+
+// Basic Idea taken from http://www.aspsnippets.com/Articles/Display-daily-Weather-Forecast-using-Weather-API-in-ASPNet.aspx
+
+namespace WebApplication3
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void GetWeatherInfo(object sender, EventArgs e)
+        {
+            string appId = "<App Id>";
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&cnt=1&APPID={1}", txtCity.Text.Trim(), appId);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                WeatherInfo weatherInfo = (new JavaScriptSerializer()).Deserialize<WeatherInfo>(json);
+                lblCity_Country.Text = weatherInfo.city.name + "," + weatherInfo.city.country;
+                imgCountryFlag.ImageUrl = string.Format("http://openweathermap.org/images/flags/{0}.png", weatherInfo.city.country.ToLower());
+                lblDescription.Text = weatherInfo.list[0].weather[0].description;
+                imgWeatherIcon.ImageUrl = string.Format("http://openweathermap.org/img/w/{0}.png", weatherInfo.list[0].weather[0].icon);
+                lblTempMin.Text = string.Format("{0}°С", Math.Round(weatherInfo.list[0].temp.min, 1));
+                lblTempMax.Text = string.Format("{0}°С", Math.Round(weatherInfo.list[0].temp.max, 1));
+                lblTempDay.Text = string.Format("{0}°С", Math.Round(weatherInfo.list[0].temp.day, 1));
+                lblTempNight.Text = string.Format("{0}°С", Math.Round(weatherInfo.list[0].temp.night, 1));
+                lblHumidity.Text = weatherInfo.list[0].humidity.ToString();
+                tblWeather.Visible = true;
+
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class WeatherInfo
+    {
+        public City city { get; set; }
+        public List<List> list { get; set; }
+    }
+
+    public class City
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Temp
+    {
+        public double day { get; set; }
+        public double min { get; set; }
+        public double max { get; set; }
+        public double night { get; set; }
+    }
+
+    public class Weather
+    {
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class List
+    {
+        public Temp temp { get; set; }
+        public int humidity { get; set; }
+        public List<Weather> weather { get; set; }
+    }
+
+
+}

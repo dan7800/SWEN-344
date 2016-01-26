9c3e635480f0ffe574343fb4a9af32afe7561cce
Brian Besmanoff <bbesmanoff@gmail.com>
2016-01-26 10:22:21 -0500

    Update the GET/POST activity

---
+++ Examples/Get_Post/get_target.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	
+</head>
+<body>
+  <p>First Name: <?php echo $_GET['fname']; ?></p>
+  <p>Last Name: <?php echo $_GET['lname']; ?></p>
+
+  <p>Notice how information is appearing in the URL header?</p>
+</body>
+</html>
---
+++ Examples/Get_Post/post_target.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	
+</head>
+<body>
+  <p>First Name: <?php echo $_POST['fname']; ?></p>
+  <p>Last Name: <?php echo $_POST['lname']; ?></p>
+
+  <p>Notice how no information is appearing in the URL header?</p>
+</body>
+</html>
